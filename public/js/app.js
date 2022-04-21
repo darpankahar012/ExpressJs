66522bbb523d66458f3d05243cfee27c3818fc2c
darpan@webmynesystems.com <darpan@webmynesystems.com>
2022-04-21 10:35:05 +0530

    update design

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-// console.log('Client side javascript file is loaded!')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -18,13 +14,10 @@ weatherForm.addEventListener('submit', (e) => {
     fetch("/weather?address=" + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                // console.log("data error", data.error)
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.location
                 messageSecond.textContent = data.forecast
-                // console.log("data location", data.location)
-                // console.log("data forecast", data.forecast)
             }
         })
     })
--- src/App.js
+++ src/App.js
@@ -21,14 +21,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Darpan Kahar'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Darpan Kahar'
     })
 })
 
@@ -36,7 +36,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: "Help",
-        name: 'Andrew Mead'
+        name: 'Darpan Kahar'
     })
 })
 
@@ -74,7 +74,7 @@ app.get('*', (req, res) => {
     res.render("404", {
         errorMessage: 'Page not found.',
         title: "Error",
-        name: 'Andrew Mead'
+        name: 'Darpan Kahar'
     })
 })
 
